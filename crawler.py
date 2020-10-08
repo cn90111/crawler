4aedacfc7fcd503f4e6312f344ad4c471a8e0d89
bob <cn90111@gmail.com>
2020-10-08 10:05:08 +0800

    feat: 新增刪除指定元素功能

--- crawler.py
+++ crawler.py
@@ -2,8 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 class Crawler():    
-    def __init__(self, url):
+    def __init__(self, url, headers=None, proxies=None):
         self.url = url
+        
+        self.headers = headers
+        self.proxies = proxies
 
     def set_url(self, url):
         self.url = url
@@ -12,11 +15,11 @@ class Crawler():
         return self.url
 
     def get_response(self):
-        return requests.get(self.url)
+        return requests.get(self.url, proxies=self.proxies, headers=self.headers)
 
     def get_select_element(self, select_tag, full_text=None):
         if not full_text:
-            full_text = self.get_full_text()        
+            full_text = self.get_full_text()
         soup = BeautifulSoup(full_text, "html.parser")
         element = soup.select(select_tag)
         return element
@@ -31,4 +34,13 @@ class Crawler():
         return False
     
     def login(self, login_url, login_parameter):
-        requests.post(login_url, login_parameter)
+        response = requests.post(login_url, login_parameter, proxies=self.proxies, headers=self.headers)
+        
+    def remove_html(self, select_tag, full_text=None):
+        if not full_text:
+            full_text = self.get_full_text()
+        soup = BeautifulSoup(full_text, "html.parser")
+        delete_elements = soup.select(select_tag)
+        for element in delete_elements:
+            element.decompose()
+        return soup
