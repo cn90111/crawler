1b7f1a0d4996fb31747ade17ecaae4662670ca2f
bob <cn90111@gmail.com>
2020-10-27 16:20:26 +0800

    feat: 加入ReadMe

---
+++ README.md
@@ -0,0 +1,22 @@
+# 環境安裝
+## 爬蟲(crawler.py)  
+`pip install requests`  
+`pip install requests[socks]`  
+`pip install beautifulsoup4`  
+## 分詞與斷詞(language_process.py)  
+`pip install nltk`
+``` python
+#在cmd輸入python，進入python環境後輸入
+import nltk
+nltk.download('punkt')
+```
+## 關鍵字查找(key_word.py)
+`pip install --upgrade gensim`  
+```
+GoogleNews-vectors-negative300.bin download
+
+https://github.com/mmihaltz/word2vec-GoogleNews-vectors
+```
+
+# API
+[Wiki](https://github.com/cn90111/crawler/wiki)
--- crawler.py
+++ crawler.py
@@ -11,23 +11,25 @@ class Crawler():
     def set_url(self, url):
         self.url = url
 
-    def get_url(self, url):
+    def get_url(self):
         return self.url
 
     def get_response(self):
         return requests.get(self.url, proxies=self.proxies, headers=self.headers)
 
-    def get_select_element(self, select_tag, full_text=None):
+    def get_select_element_list(self, select_tag, full_text=None):
         if not full_text:
             full_text = self.get_response().text
         soup = BeautifulSoup(full_text, "html.parser")
-        element = soup.select(select_tag)
-        return element
+        element_list = soup.select(select_tag)
+        return element_list
 
     def get_next_element(self, element, neighbor_tag):
         return element.find_next(neighbor_tag)
 
     def need_login(self, select_tag, full_text=None):
+        if not full_text:
+            full_text = self.get_response().text
         login_button = self.get_select_element(select_tag, full_text=full_text)
         if login_button:
             return True
