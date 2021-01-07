a6f035c089d01b05407a576024cf558a5598d5b9
cn90111 <cn90111@gmail.com>
2021-01-07 12:00:53 +0800

    feat: 加入 git flow 管理
    調整項目：
    1. 加入驗證碼處理機制
    2. 新增爬蟲程式碼
    3. 新增 keyword 處理的程式碼
    
    issue #2

--- key_word.py
+++ key_word.py
@@ -1,13 +1,21 @@
 #https://www.itread01.com/content/1549674027.html
 
 from gensim.models.keyedvectors import KeyedVectors
+from gensim.models import word2vec
 
 class KeyWord():
-    def __init__(self, loading_model=True):
+    def __init__(self, mode='english', loading_model=True):
         self.model = None
+        self.MODEL_LIST = ['english', 'traditional_chinese']
+        
         if loading_model:
             print('loading model')
-            self.model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+            if mode == 'english':
+                self.model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+            elif mode == 'traditional_chinese':
+                self.model = word2vec.Word2Vec.load("20180309wiki_model.bin")
+            else:
+                raise AttributeError('mode unsupport, support list: '+str(self.MODEL_LIST))
             print('loading finish')
         
     def get_similarity(self, key_word, check_word):
--- language_process.py
+++ language_process.py
@@ -3,10 +3,41 @@ Bird, Steven, Edward Loper and Ewan Klein (2009).
 Natural Language Processing with Python.  O'Reilly Media Inc.
 '''
 import nltk
+import jieba
+import re
+from translate import Translator
 
 class LanguageProcess():
+    def __init__(self, mode='english'):
+        self.MODE = ['traditional_chinese', 'english', 'simplified_chinese']
+        if mode:
+            if mode not in self.MODE:
+                raise AttributeError('mode unsupport, mode list: '+str(self.MODE))
+            elif mode == 'traditional_chinese':
+                jieba.set_dictionary('dict.txt.big')
+        self.mode = mode
+        
     def get_sentence(self, text):
-        return nltk.sent_tokenize(text)
+        sentence_list = None
+        if self.mode == 'english':
+            sentence_list = nltk.sent_tokenize(text)
+        elif self.mode == 'traditional_chinese' or self.mode == 'simplified_chinese':
+            raise AttributeError('chinese unsupport')
+        return sentence_list
         
     def get_word(self, text):
-        return nltk.word_tokenize(text)
+        word_list = None
+        if self.mode == 'english':
+            word_list = nltk.word_tokenize(text)
+        elif self.mode == 'traditional_chinese' or self.mode == 'simplified_chinese':
+            word_list = jieba.cut(text, cut_all=False)
+        return word_list
+        
+    # need network
+    def translate(self, word, from_lang, to_lang='english'):
+        translator= Translator(from_lang=from_lang,to_lang=to_lang)
+        return translator.translate(word)
+        
+    def is_english(self, word):
+        english_check = r'[a-zA-Z]+'
+        return re.fullmatch(english_check, word)
